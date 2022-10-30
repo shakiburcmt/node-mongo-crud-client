aa23d6ea51d695f1de283a20d5a79f46c4b804ab
shakiburcmt <shakibur.cmt@gmail.com>
2022-10-31 01:14:16 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import AddUser from './components/AddUser';
+import Home from './components/Home';
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element:<Home></Home>
+    },
+    {
+      path: '/users/add',
+      element:<AddUser></AddUser>
+    }
+  ])
+  
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
